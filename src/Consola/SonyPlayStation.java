d929a4b6937092ed75c6425ce61f8e00c9480030
ZevsP <sermarser31@gmail.com>
2024-09-19 19:06:04 +0200

    second commit

---
+++ src/Consola/SonyPlayStation.java
@@ -0,0 +1,4 @@
+package Consola;
+
+public class Consola {
+}
--- src/Videojuego/Consola.java
+++ src/Videojuego/Consola.java
@@ -1,10 +1,12 @@
+package Videojuego;
+
 public interface InterfaceVideojuego {
     void iniciarVideojuego(String tipoConsola, String tipoJuego, String nombreJuego, double precioJuego);
     void finalizarVideojuego();
     void installarVideojuego(Videojuego videojuego) throws VideojuegoNoCompatibleException;
-    void playVideojuego(Videojuego videojuego);
+    void playVideojuego();
 
-};
+
 
 
 
